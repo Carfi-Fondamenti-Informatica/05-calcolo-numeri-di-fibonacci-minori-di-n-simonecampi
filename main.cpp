8c96b9b2f492c89d4dc27a713a21361ad986c5c0
simonecampi <118023444+simonecampi@users.noreply.github.com>
2022-11-28 08:35:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   int a=1, b=1, c=0, n=0;
+   cin>>n;
+   cout<<a<<endl;
+   cout<<b<<endl;
+   while (c<=n)
+   {
+      c = a + b;
+      if (c<=n) {cout << c << endl;}
+      a = b;
+      b = c;
+   }
+
    return 0;
 }
